32c6bcbd2edd359367385723b9fef6256d599c0d
Shahood <shahoodislam2@gmail.com>
2026-04-24 09:33:58 +0500

    Refactor service worker for caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,16 +1,44 @@
-self.addEventListener("install", e=>{
-    e.waitUntil(
-        caches.open("glow-cache").then(cache=>{
-            return cache.addAll([
-                "/",
-                "index.html"
-            ]);
-        })
-    );
+const CACHE_NAME = "glow-beauty-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./icon.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", e=>{
-    e.respondWith(
-        caches.match(e.request).then(res=>res || fetch(e.request))
-    );
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
